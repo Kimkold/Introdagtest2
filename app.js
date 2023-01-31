58771e65d60a05ff9a24c630cf259bb5e8c1c1df
Kimkold <Valdehannibal@hotmail.com>
2023-01-31 12:48:04 +0100

    Ændringer

---
+++ app.js
@@ -0,0 +1,6 @@
+document.body.innerHTML += "hello javascript!";
+function sayHello() {
+  document.querySelector("h3").textContent =
+    "Hej " + document.querySelector("input").value;
+}
+document.querySelector("button").addEventListener("click", sayHello);
