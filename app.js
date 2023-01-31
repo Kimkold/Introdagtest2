c70c545153695b0be889b952052a7aad1bc50059
Kimkold <Valdehannibal@hotmail.com>
2023-01-31 13:29:53 +0100

    dobbelt tekstbox

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-document.body.innerHTML += "hello javascript!";
 function sayHello() {
   document.querySelector("h3").textContent =
-    "Hej " + document.querySelector("input").value;
+    "Hej " + document.querySelector("#name ").value;
+  document.querySelector("h3").textContent +=
+    " din alder er " + document.querySelector("#age").value;
 }
 document.querySelector("button").addEventListener("click", sayHello);
